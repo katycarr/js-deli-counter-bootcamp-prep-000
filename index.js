c22735ffebf408c577ac12f8d9a63a15be3be38b
Katy Carr <khcarr9@gmail.com>
2017-10-19 23:21:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ var katzDeli = [];
 function takeANumber(katzDeliLine,name) {
   var position = katzDeliLine.length+1;
   katzDeliLine.push(name);
-  return position;
+  return `Welcome, ${name}. You are number ${position} in line.`
 }
+
